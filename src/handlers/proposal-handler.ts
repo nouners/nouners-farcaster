9a54497e28f9203d02abeabd4d4ee6b24768b9ef
Milad Nekofar <nekofar@gmail.com>
2025-11-10 12:45:40 +0300

    chore: resolve lint findings

--- src/handlers/proposal-handler.ts
+++ src/handlers/proposal-handler.ts
@@ -13,6 +13,7 @@ import { logger } from '@/utilities/logger'
 import { DateTime } from 'luxon'
 import { createHash } from 'node:crypto'
 import { filter, isTruthy, map, pipe } from 'remeda'
+import { ProposalStatus } from '@nekofar/nouns/subgraphs'
 
 interface DirectCastBody {
   type: 'direct-cast'
@@ -73,7 +74,8 @@ export async function proposalHandler(env: Env) {
   proposals = filter(
     proposals,
     (proposal) =>
-      proposal.status === 'ACTIVE' && Number(proposal.endBlock) > blockNumber,
+      proposal.status === ProposalStatus.Active &&
+      Number(proposal.endBlock) > blockNumber,
   )
 
   logger.info(
